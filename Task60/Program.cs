571b80f60f80c0245a9be8ff5be0fa26c73dea4b
Vladimir Shytsko <shytsko.v@gmail.com>
2022-05-18 23:08:17 +0300

    Add Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,48 @@
+﻿// **Задача 60**: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу,
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int dimension1 = ReadInteger("Введите размер 1: ");
+int dimension2 = ReadInteger("Введите размер 2: ");
+int dimension3 = ReadInteger("Введите размер 3: ");
+int count2DNumbers = 89;
+
+if (dimension1 * dimension2 * dimension3 <= count2DNumbers)
+{
+    int[,,] array = CreateArray3D(dimension1, dimension2, dimension3);
+    for (int d1 = 0; d1 < array.GetLength(0); d1++)
+        for (int d2 = 0; d2 < array.GetLength(1); d2++)
+            for (int d3 = 0; d3 < array.GetLength(2); d3++)
+                Console.WriteLine($"array[{d1}, {d2}, {d3}] = {array[d1, d2, d3]}");
+}
+else
+{
+    Console.WriteLine("Невозможно заполнить массив двухзначными неповторяющимися числами");
+}
+
+int[,,] CreateArray3D(int size1, int size2, int size3)
+{
+    int[,,] array = new int[size1, size2, size3];
+
+    int value = 10;
+    int[] twoDigitValues = new int[89];
+    for (int i = 0; i < twoDigitValues.Length; i++)
+        twoDigitValues[i] = value++;
+
+    int minValueIndex = 0;
+    for (int d1 = 0; d1 < array.GetLength(0); d1++)
+        for (int d2 = 0; d2 < array.GetLength(1); d2++)
+            for (int d3 = 0; d3 < array.GetLength(2); d3++)
+            {
+                int valueIndex = new Random().Next(minValueIndex, twoDigitValues.Length);
+                array[d1, d2, d3] = twoDigitValues[valueIndex];
+                twoDigitValues[valueIndex] = twoDigitValues[minValueIndex];
+                minValueIndex++;
+            }
+    return array;
+}
+
+int ReadInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
